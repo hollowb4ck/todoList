7ced26aa0f0c6a0fc4e57f03e2ddb09ef1db3e06
holowb4ck <wuta322@gmail.com>
2017-05-18 12:50:20 +0300

    todo list is alive

---
+++ index.js
@@ -0,0 +1,113 @@
+import { Map, List } from 'immutable';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import ReactDOM from 'react-dom';
+import React from 'react'
+
+const components = {
+  Todo({ todo }) {
+    if(todo.isDone) {
+      return <span></span>;
+    } else {
+      return <span>{todo.text}</span>;
+    }
+  },
+  TodoList({ todos, toggleTodo, addTodo }) {
+    const onSubmit = (e) => {
+      const text = e.target.value;
+      if(e.which === 13 && text.length > 0) {
+        addTodo(text);
+        e.target.value = '';
+      }
+    };
+
+    const toggleClick = (id) => () => toggleTodo(id);
+
+    const { Todo } = components;
+
+    return (
+      <div className='todo'>
+        <input type='text'
+               className='todo__entry'
+               placeholder='Add todo'
+               onKeyDown={onSubmit} />
+        <ul className='todo__list'>
+          {todos.map(t => (
+            <li
+              key={t.get('id')}
+              className='todo__item'
+              onClick={toggleClick(t.get('id'))}>
+              <Todo todo={t.toJS()} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
+
+const actions = {
+  addTodo(text) {
+    return {
+      type: 'ADD_TODO',
+      payload: {
+        id: Math.random().toString(34).slice(2),
+        isDone: false,
+        text
+      }
+    };
+  },
+  toggleTodo(id) {
+    return {
+      type: 'TOGGLE_TODO',
+      payload: id
+    }
+  }
+};
+
+const init = List();
+
+const reducer = function(state=init, action) {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return state.push(
+        Map(action.payload)
+      );
+    case 'TOGGLE_TODO':
+      return state.map(t => {
+        if(t.get('id') == action.payload) {
+          return t.update('isDone', isDone => !isDone);
+        } else {
+          return t;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+const containers = {
+  TodoList: connect(
+    function mapStateToProps(state) {
+      return {
+        todos: state
+      };
+    },
+    function mapDispatchToProps(dispatch) {
+      return {
+        toggleTodo: (id) => dispatch(actions.toggleTodo(id)),
+        addTodo: (text) => dispatch(actions.addTodo(text))
+      };
+    }
+  )(components.TodoList)
+};
+
+const { TodoList } = containers;
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoList />
+  </Provider>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+      {
+        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+        loader: 'file-loader'
+      }
+    ]
+  }
+}
